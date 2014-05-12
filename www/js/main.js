79eeda3ba718807045d2706ef5023c13e982e7e8
raphaelstary <raphael.stary@gmail.com>
2014-05-12 11:11:35 +0200

    split application initialization into sub functions

--- www/js/main.js
+++ www/js/main.js
@@ -1,20 +1,32 @@
 window.onload = function () {
     "use strict";
 
+    function installResizeHandler(rAF, resizeBus) {
+        var screenSizer = new ScreenSizer(resizeBus, window.innerWidth, window.innerHeight),
+            resizeHandler = new ResizeHandler(screenSizer, rAF);
+
+        window.addEventListener('resize', resizeHandler.handleResize.bind(resizeHandler));
+    }
+
+    function installInputListeners(screenInput) {
+        screen.addEventListener('touchstart', screenInput.touchStart.bind(screenInput));
+        screen.addEventListener('click', screenInput.click.bind(screenInput));
+    }
+
+    function startApp(rAF, resizeBus, screenInput) {
+        var screen = document.getElementById('screen'),
+            ctx = screen.getContext('2d'),
+            app = new App(screen, ctx, rAF, resizeBus, screenInput);
+
+        app.start(window.innerWidth, window.innerHeight);
+    }
+
     var rAF = window.requestAnimationFrame.bind(window),
-        screen = document.getElementById('screen'),
-        ctx = screen.getContext('2d'),
         resizeBus = new ResizeBus(),
-        screenSizer = new ScreenSizer(resizeBus, window.innerWidth, window.innerHeight),
-        screenInput = new TapController(),
-        app = new App(screen, ctx, rAF, resizeBus, screenInput),
-        resizeHandler = new ResizeHandler(screenSizer, rAF);
+        screenInput = new TapController();
 
+    installResizeHandler(rAF, resizeBus);
+    installInputListeners(screenInput);
+    startApp(rAF, resizeBus, screenInput);
 
-    window.addEventListener('resize', resizeHandler.handleResize.bind(resizeHandler));
-
-    screen.addEventListener('touchstart', screenInput.touchStart.bind(screenInput));
-    screen.addEventListener('click', screenInput.click.bind(screenInput));
-
-    app.start(window.innerWidth, window.innerHeight);
 };
