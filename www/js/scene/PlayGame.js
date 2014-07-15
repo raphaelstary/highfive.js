b63280951e9d1109c7ac31d8910628f3a0eaabf6
Raphael Stary <raphael.stary@gmail.com>
2014-07-15 10:33:38 +0200

    extract counter animation to odometer component class

--- www/js/scene/PlayGame.js
+++ www/js/scene/PlayGame.js
@@ -1,4 +1,4 @@
-var PlayGame = (function (Transition, ScreenShaker, LevelGenerator) {
+var PlayGame = (function (Transition, ScreenShaker, LevelGenerator, Odometer) {
     "use strict";
 
     function PlayGame(stage, sceneStorage, gameLoop, gameController) {
@@ -29,6 +29,8 @@ var PlayGame = (function (Transition, ScreenShaker, LevelGenerator) {
 
         countDrawables.forEach(shaker.add.bind(shaker));
         speedStripes.forEach(shaker.add.bind(shaker));
+
+        var scoreDisplay = new Odometer(this.stage, countDrawables);
 
         var shieldsOn = false; //part of global game state
         var lives = 3; //3; //part of global game state
@@ -129,7 +131,9 @@ var PlayGame = (function (Transition, ScreenShaker, LevelGenerator) {
                 if (needPreciseCollisionDetection(star) && isHit(star)) {
                     collectStar();
                     showScoredPoints(star.x, star.y);
-                    increaseTotalScore(10);
+                    var score = 10;
+                    scoreDisplay.addScore(score);
+                    points += score;
 
                     self.stage.remove(star);
                     delete trackedStars[key];
@@ -176,75 +180,6 @@ var PlayGame = (function (Transition, ScreenShaker, LevelGenerator) {
                 self.stage.remove(scoredPointsDrawable);
             });
         }
-
-        var sprite0_1, sprite1_2, sprite2_3, sprite3_4, sprite4_5, sprite5_6, sprite6_7, sprite7_8, sprite8_9, sprite9_0;
-        var countSprites;
-        var countStatics;
-
-        function initIncreaseTotalScoreRenderStuff() {
-
-            sprite0_1 = self.stage.getSprite('0_1-anim/0_1', 15, false);
-            sprite1_2 = self.stage.getSprite('1_2-anim/1_2', 15, false);
-            sprite2_3 = self.stage.getSprite('2_3-anim/2_3', 15, false);
-            sprite3_4 = self.stage.getSprite('3_4-anim/3_4', 15, false);
-            sprite4_5 = self.stage.getSprite('4_5-anim/4_5', 15, false);
-            sprite5_6 = self.stage.getSprite('5_6-anim/5_6', 15, false);
-            sprite6_7 = self.stage.getSprite('6_7-anim/6_7', 15, false);
-            sprite7_8 = self.stage.getSprite('7_8-anim/7_8', 15, false);
-            sprite8_9 = self.stage.getSprite('8_9-anim/8_9', 15, false);
-            sprite9_0 = self.stage.getSprite('9_0-anim/9_0', 15, false);
-            countSprites = [sprite0_1, sprite1_2, sprite2_3, sprite3_4, sprite4_5, sprite5_6, sprite6_7, sprite7_8, sprite8_9, sprite9_0];
-
-            countStatics = [self.stage.getSubImage('num/numeral0'), self.stage.getSubImage('num/numeral1'), self.stage.getSubImage('num/numeral2'),
-                self.stage.getSubImage('num/numeral3'), self.stage.getSubImage('num/numeral4'), self.stage.getSubImage('num/numeral5'),
-                self.stage.getSubImage('num/numeral6'), self.stage.getSubImage('num/numeral7'), self.stage.getSubImage('num/numeral8'),
-                self.stage.getSubImage('num/numeral9')];
-        }
-
-        initIncreaseTotalScoreRenderStuff();
-
-        var totalScore = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-        function increaseTotalScore(score) {
-            points += score;
-            var scoreString = score.toString();
-
-            var u = 0,
-                overflow = 0;
-
-            for (var i = scoreString.length - 1; i > -1; i--) {
-                addDigit(parseInt(scoreString[i], 10));
-            }
-            while (overflow > 0) {
-                addDigit(0);
-            }
-
-            function addDigit(intToAdd) {
-                var currentDigit = totalScore[u];
-                var tmpAmount = currentDigit + intToAdd + overflow;
-                overflow = Math.floor(tmpAmount / 10);
-                var newDigit = tmpAmount % 10;
-
-                var delta = tmpAmount - currentDigit;
-                var currentDrawable = countDrawables[u];
-                for (var v = 0; v < delta; v++) {
-                    var currentSprite = countSprites[(currentDigit + v) % 10];
-                    (function (currentDrawable, currentDigit, v) {
-                        self.stage.animate(currentDrawable, currentSprite, function () {
-                            currentDrawable.img = countStatics[(currentDigit + 1 + v) % 10];
-                        })
-                    })(currentDrawable, currentDigit, v);
-                    if ((currentDigit + v) % 10 === newDigit) {
-                        break;
-                    }
-                }
-
-                totalScore[u] = newDigit;
-
-                u++;
-            }
-        }
-
 
         var collectSprite;
 
@@ -469,4 +404,4 @@ var PlayGame = (function (Transition, ScreenShaker, LevelGenerator) {
     };
 
     return PlayGame;
-})(Transition, ScreenShaker, LevelGenerator);
+})(Transition, ScreenShaker, LevelGenerator, Odometer);
