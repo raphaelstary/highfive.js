5017f7f16f843b8f36d1a4f89b58fb60a8188652
Raphael Stary <raphael.stary@gmail.com>
2014-07-26 23:06:05 +0200

    changed credits screen rendering to use responsive constants instead of fix values

--- www/js/scene/Credits.js
+++ www/js/scene/Credits.js
@@ -1,4 +1,4 @@
-var Credits = (function (Transition, window) {
+var Credits = (function (Transition, window, calcScreenConst) {
     "use strict";
 
     function Credits(stage, tapController, messages) {
@@ -7,13 +7,12 @@ var Credits = (function (Transition, window) {
         this.messages = messages;
     }
 
-    Credits.prototype.show = function (nextScene, previousScenesDrawables) {
+    Credits.prototype.show = function (nextScene, previousScenesDrawables, screenWidth, screenHeight) {
         var self = this;
-        var length = 320;
 
         function fadeOut(drawables) {
             drawables.forEach(function (drawable) {
-                var path = self.stage.getPath(drawable.x, drawable.y, drawable.x - length, drawable.y, 60,
+                var path = self.stage.getPath(drawable.x, drawable.y, drawable.x - screenWidth, drawable.y, 60,
                     Transition.EASE_IN_OUT_QUAD);
                 self.stage.move(drawable, path);
             });
@@ -21,56 +20,83 @@ var Credits = (function (Transition, window) {
 
         function fadeIn(drawables) {
             drawables.forEach(function (drawable) {
-                var path = self.stage.getPath(drawable.x, drawable.y, drawable.x + length, drawable.y, 60,
+                var path = self.stage.getPath(drawable.x, drawable.y, drawable.x + screenWidth, drawable.y, 60,
                     Transition.EASE_IN_OUT_QUAD);
                 self.stage.move(drawable, path);
             });
         }
         var back;
+        var screenWidthHalf = calcScreenConst(screenWidth, 2);
+        var screenHeightHalf = calcScreenConst(screenHeight, 2);
+        var screenHeightQuarter = calcScreenConst(screenHeight, 4);
+        var offSet = screenWidth;
+
+        var fb, twitter, letsplayIO;
         function drawCreditsScreen() {
-            var offSet = length;
-            var topRaster = 480 / 100 * 8;
-            back = self.stage.drawFresh(320 / 10 + offSet, topRaster, 'back');
+            var topRaster = calcScreenConst(screenHeight, 25, 2);
+            back = self.stage.drawFresh(calcScreenConst(screenWidth, 10) + offSet, topRaster, 'back');
 
             var KEN_PIXEL_BLOCKS = 'KenPixelBlocks';
             var LIGHT_GREY = '#c4c4c4';
-            var a_txt = self.stage.getDrawableText(320 / 2 + offSet, 480 / 4 - 40, 3, self.messages.get('credits', 'a'),
-                25, KEN_PIXEL_BLOCKS, LIGHT_GREY);
-            self.stage.draw(a_txt);
 
-            var game_txt = self.stage.getDrawableText(320 / 2 + offSet, 480 / 4, 3, self.messages.get('credits', 'game'),
-                45, KEN_PIXEL_BLOCKS, LIGHT_GREY);
+            var game_txt = self.stage.getDrawableText(screenWidthHalf + offSet, screenHeightQuarter, 3,
+                self.messages.get('credits', 'game'), 45, KEN_PIXEL_BLOCKS, LIGHT_GREY);
             self.stage.draw(game_txt);
 
-            var by_txt = self.stage.getDrawableText(320 / 2 + offSet, 480 / 4 + 40, 3, self.messages.get('credits', 'by'),
-                25, KEN_PIXEL_BLOCKS, LIGHT_GREY);
+            var a_txt = self.stage.getDrawableText(screenWidthHalf + offSet, screenHeightQuarter - game_txt.txt.size, 3,
+                self.messages.get('credits', 'a'), 25, KEN_PIXEL_BLOCKS, LIGHT_GREY);
+            self.stage.draw(a_txt);
+
+            var by_txt = self.stage.getDrawableText(screenWidthHalf + offSet, screenHeightQuarter + game_txt.txt.size,
+                3, self.messages.get('credits', 'by'), 25, KEN_PIXEL_BLOCKS, LIGHT_GREY);
             self.stage.draw(by_txt);
 
-            var letsplayIO = self.stage.drawFresh(320 / 2 + offSet, 480 / 2, 'credits-logo');
-            var fb = self.stage.drawFresh(320 / 2 - 18 + offSet, 480 / 2 + 40, 'credits-facebook');
-            var twitter = self.stage.drawFresh(320 / 2 + 18 + offSet, 480 / 2 + 40, 'credits-twitter');
+            letsplayIO = self.stage.drawFresh(screenWidthHalf + offSet, screenHeightHalf, 'credits-logo');
+            var socialBtnOffSet = calcScreenConst(self.stage.getSubImage('credits-facebook').width, 5, 4);
+            var logoMarginBottom = calcScreenConst(letsplayIO.img.height, 2, 3);
+            fb = self.stage.drawFresh(screenWidthHalf - socialBtnOffSet + offSet,
+                screenHeightHalf + logoMarginBottom, 'credits-facebook');
+            twitter = self.stage.drawFresh(screenWidthHalf + socialBtnOffSet + offSet,
+                screenHeightHalf + logoMarginBottom, 'credits-twitter');
 
-            var graphics_txt = self.stage.getDrawableText(320 / 2 + offSet, 480 / 4 * 3, 3, self.messages.get('credits', 'graphics'),
-                20, KEN_PIXEL_BLOCKS, LIGHT_GREY);
+            var graphics_txt = self.stage.getDrawableText(screenWidthHalf + offSet, screenHeightQuarter * 3, 3,
+                self.messages.get('credits', 'graphics'), 20, KEN_PIXEL_BLOCKS, LIGHT_GREY);
             self.stage.draw(graphics_txt);
-            var kenney = self.stage.drawFresh(320 / 2 + offSet, 480 / 4 * 3 + 50, 'kenney');
+            var gfxMarginBottom = calcScreenConst(self.stage.getSubImage('kenney').height, 6, 5);
+            var kenney = self.stage.drawFresh(screenWidthHalf + offSet, graphics_txt.y + gfxMarginBottom,
+                'kenney');
 
             return [back, a_txt, game_txt, by_txt, letsplayIO, fb, twitter, graphics_txt, kenney];
         }
 
         function registerTapListener() {
-            var twitterTouchable = {id: 'twitter_letsplayIO_tap', x: 320 / 2 + 18 - 20, y: 480 / 2 + 40 - 20,
-                width: 40, height: 40};
+            var twitterTouchable = {
+                id: 'twitter_letsplayIO_tap',
+                x: twitter.getCornerX() - offSet,
+                y: twitter.getCornerY(),
+                width: twitter.getWidth(),
+                height: twitter.getHeight()
+            };
             self.tapController.add(twitterTouchable, function () {
                 window.open('https://twitter.com/letsplayIO', '_blank');
             });
-            var fbTouchable = {id: 'fb_letsplayIO_tap', x: 320 / 2 - 18 - 20, y: 480 / 2 + 40 - 20,
-                width: 40, height: 40};
+            var fbTouchable = {
+                id: 'fb_letsplayIO_tap',
+                x: fb.getCornerX() - offSet,
+                y: fb.getCornerY(),
+                width: fb.getWidth(),
+                height: fb.getHeight()
+            };
             self.tapController.add(fbTouchable, function () {
                 window.open('https://www.facebook.com/letsplayIO', '_blank');
             });
-            var letsplayIOSiteTouchable = {id: 'letsplayIO_site_tap', x: 320 / 2 - 100, y: 480 / 2 - 20,
-                width: 200, height: 40};
+            var letsplayIOSiteTouchable = {
+                id: 'letsplayIO_site_tap',
+                x: letsplayIO.getCornerX() - offSet,
+                y: letsplayIO.getCornerY(),
+                width: letsplayIO.getWidth(),
+                height: letsplayIO.getHeight()
+            };
             self.tapController.add(letsplayIOSiteTouchable, function () {
                 window.open('http://letsplay.io/', '_blank');
             });
@@ -83,10 +109,17 @@ var Credits = (function (Transition, window) {
 
         fadeOut(previousScenesDrawables);
         var creditsDrawables = drawCreditsScreen();
-        fadeOut(creditsDrawables);
         var touchables = registerTapListener();
+        fadeOut(creditsDrawables);
 
-        var backTouchable = {id: 'back_tap', x: 0, y: 0, width: 320 / 4, height: 480 / 4};
+
+        var backTouchable = {
+            id: 'back_tap',
+            x: 0,
+            y: 0,
+            width: calcScreenConst(screenWidth, 4),
+            height: screenHeightQuarter
+        };
         self.tapController.add(backTouchable, endScene);
         touchables.push(backTouchable);
 
@@ -112,4 +145,4 @@ var Credits = (function (Transition, window) {
     };
 
     return Credits;
-})(Transition, window);
+})(Transition, window, calcScreenConst);
