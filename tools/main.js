373d8e0a18521d7dc8e34d64e748a1ef81773c0c
Raphael Stary <raphael.stary@gmail.com>
2014-08-14 00:06:46 +0200

    refactoring + verbose feature

--- tools/main.js
+++ tools/main.js
@@ -16,85 +16,126 @@ var UglifyJS = require("uglify-js"),
     Minimize = require('minimize'),
     minimize = new Minimize();
 
-fs.readFile(WWW + '/' + INDEX, 'utf8', function (error, data) {
-    if (error) {
-        return console.log('error reading index file: ' + error);
-    }
+buildTheProject();
 
-    var $ = cheerio.load(data);
+function buildTheProject() {
+    loadIndexFile(function (error, data) {
+        if (error) {
+            return console.log('error reading index file: ' + error);
+        }
 
-    var minifiedJS = minifyJS($);
-    var minifiedCSS = minifyCSS($);
-    var newIndex = updateSources($);
+        mkdir(WWW_BUILT, function () {
 
-    mkdir(WWW_BUILT, function () {
+            var $ = loadDocumentDOM(data);
 
-        writeJsFile(minifiedJS);
-        writeCssFile(minifiedCSS);
-        writeIndexFile(newIndex);
-        cpdir(WWW + '/' + GFX, WWW_BUILT + '/' + GFX);
-        cpdir(WWW + '/' + DATA, WWW_BUILT + '/' + DATA);
+            var minifiedJS = minifyJS($);
+            writeJsFile(minifiedJS);
 
+            var minifiedCSS = minifyCSS($);
+            writeCssFile(minifiedCSS);
+
+            var newIndex = updateSources($);
+            minifyHtml(newIndex,
+                writeIndexFile);
+
+            cpdir(WWW + '/' + GFX, WWW_BUILT + '/' + GFX);
+            cpdir(WWW + '/' + DATA, WWW_BUILT + '/' + DATA);
+
+        });
     });
-});
+}
+
+function loadDocumentDOM(data) {
+    console.log('parsing DOM');
+    return cheerio.load(data);
+}
+
+function loadIndexFile(callback) {
+    console.log('loading index file');
+    fs.readFile(WWW + '/' + INDEX, 'utf8', callback);
+}
 
 function getExtension(fileName) {
     return fileName.split('.').pop();
 }
 
+function cp(srcPath, file, destPath) {
+    console.log('copy ' + srcPath + '/' + file + " to " + destPath);
+    fs.createReadStream(srcPath + '/' + file).pipe(fs.createWriteStream(destPath + '/' + file));
+}
+function isJSON(file) {
+    return getExtension(file) == 'json';
+}
+
+function minifyJSONFile(srcPath, fileName, callback) {
+    console.log('start task: minify ' + fileName);
+
+    fs.readFile(srcPath + '/' + fileName, 'utf8', function (error, data) {
+        var minified = minifyJSON(data);
+
+        console.log('success task: minify ' + fileName);
+
+        callback(minified);
+    });
+}
+
+function writeFile(destPath, fileName, data) {
+    console.log('start task: write ' + fileName);
+
+    fs.writeFile(destPath + '/' + fileName, data, 'utf8', function (err) {
+        if (err) {
+            return console.log("error writing file: " + err);
+        }
+        console.log('success task: write ' + fileName);
+    });
+}
+
+function cpDirFiles(srcPath, destPath) {
+    fs.readdir(srcPath, function (error, files) {
+        files.forEach(function (file) {
+            if (isJSON(file)) {
+                minifyJSONFile(srcPath, file,
+                    writeFile.bind(null, destPath, file)
+                );
+            } else {
+                cp(srcPath, file, destPath);
+            }
+        });
+    });
+}
 function cpdir(srcPath, destPath) {
     mkdir(destPath, function () {
-        fs.readdir(srcPath, function (error, files) {
-            files.forEach(function (file) {
-                if (getExtension(file) == 'json') {
-                    fs.readFile(srcPath + '/' + file, 'utf8', function (error, data) {
-                        var minified = minifyJSON(data);
-                        fs.writeFile(destPath + '/' + file, minified, 'utf8', function (err) {
-                            if (err) {
-                                return console.log("error writing minified json: " + err);
-                            }
-                        })
-                    });
-                } else {
-                    fs.createReadStream(srcPath + '/' + file).pipe(fs.createWriteStream(destPath + '/' + file));
-                }
-            });
-        });
+        cpDirFiles(srcPath, destPath);
     });
 }
 
 function mkdir(name, callback) {
-    fs.rmdir(name, function (error) {
-        fs.mkdir(name, function (error) {
+    fs.rmdir(name, function () {
+        fs.mkdir(name, function () {
             callback();
         });
     });
 }
 
-function writeJsFile(result) {
-    fs.writeFile(WWW_BUILT + '/' + MIN_JS, result, 'utf8', function (err, data) {
-        if (err) {
-            return console.log("error writing js-min: " + err);
-        }
+function writeJsFile(data) {
+    writeFile(WWW_BUILT, MIN_JS, data);
+}
+
+function writeCssFile(data) {
+    writeFile(WWW_BUILT, MIN_CSS, data);
+}
+
+function minifyHtml(data, callback) {
+    console.log("starting task: minify HTML");
+    minimize.parse(data, function (error, data) {
+
+        console.log("success task: minify HTML");
+        callback(data);
     });
 }
 
-function writeCssFile(uglified) {
-    fs.writeFile(WWW_BUILT + '/' + MIN_CSS, uglified, 'utf8', function (err, data) {
-        if (err) {
-            return console.log("error writing css-min: " + err);
-        }
-    });
-}
-
-function writeIndexFile(result) {
-    minimize.parse(result, function (error, data) {
-        fs.writeFile(WWW_BUILT + '/' + INDEX, data, 'utf8', function (err, data) {
-            if (err) {
-                return console.log("error writing index: " + err);
-            }
-        });
-    });
+function writeIndexFile(data) {
+    writeFile(WWW_BUILT, INDEX, data);
 }
 
 function getAllJsFileNames($) {
@@ -115,25 +156,40 @@ function getAllCssFileNames($) {
 }
 
 function minifyJS($) {
+    console.log("starting task: minify JS");
+
     var allScripts = getAllJsFileNames($);
-    return UglifyJS.minify(allScripts).code;
+    var minified = UglifyJS.minify(allScripts).code;
+
+    console.log('success task: minify JS');
+    return  minified;
 }
 
 function minifyCSS($) {
+    console.log("starting task: minify CSS");
+
     var allCSS = getAllCssFileNames($);
-    return UglifyCSS.processFiles(allCSS, {uglyComments: true});
+    var minified = UglifyCSS.processFiles(allCSS, {uglyComments: true});
+
+    console.log('success task: minify CSS');
+    return  minified;
 }
 
 function updateSources($) {
+    console.log("starting task: update html sources");
+
     $('script').remove();
-    $('body').append('<script src="' + MIN_JS +'"></script>');
+    $('body').append('<script src="' + MIN_JS + '"></script>');
 
     $('link').each(function () {
         if ($(this).attr('rel') == 'stylesheet')
             $(this).remove();
     });
 
-    $('head').append('<link rel="stylesheet" href="' + MIN_CSS +'">');
+    $('head').append('<link rel="stylesheet" href="' + MIN_CSS + '">');
 
-    return $.html();
+    var html = $.html();
+
+    console.log("success task: update html sources");
+    return  html;
 }
