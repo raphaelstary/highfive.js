c2ab99491fe01e1193040b80de5cbda88e3d93fb
Raphael Stary <raphael.stary@gmail.com>
2014-09-23 16:50:36 +0200

    adds scaling to drawables (cherry picked from commit c659760)

--- www/js/render/intern/entity/Drawable.js
+++ www/js/render/intern/entity/Drawable.js
@@ -1,7 +1,7 @@
 var Drawable = (function (Math, measureText) {
     "use strict";
 
-    function Drawable(id, x, y, img, txt, zIndex, alpha, rotation) {
+    function Drawable(id, x, y, img, txt, zIndex, alpha, rotation, scale) {
         this.id = id;
         this.x = x;
         this.y = y;
@@ -10,38 +10,42 @@ var Drawable = (function (Math, measureText) {
         this.zIndex = zIndex === undefined ? 3 : zIndex;
         this.rotation = rotation;
         this.alpha = alpha;
+        this.scale = scale || 1;
     }
 
     Drawable.prototype.getCornerX = function () {
-        return this.x - this.__getWidth() / 2;
+        return this.x - Math.floor(this.__getWidth() / 2 * this.scale);
     };
 
     Drawable.prototype.getCornerY = function () {
-        return this.y - this.__getHeight() / 2;
+        return this.y - Math.floor(this.__getHeight() / 2 * this.scale);
     };
 
     Drawable.prototype.getEndX = function () {
-        return this.x + this.__getWidth() / 2;
+        return this.x + Math.floor(this.__getWidth() / 2 * this.scale);
     };
 
     Drawable.prototype.getEndY = function () {
-        return this.y + this.__getHeight() / 2;
+        return this.y + Math.floor(this.__getHeight() / 2 * this.scale);
     };
 
     Drawable.prototype.getWidth = function () {
-        return this.__getWidth();
+        return Math.floor(this.__getWidth() * this.scale);
     };
 
     Drawable.prototype.getHeight = function () {
-        return this.__getHeight();
+        return Math.floor(this.__getHeight() * this.scale);
     };
 
     Drawable.prototype.__getHeight = function () {
         if (this.txt) {
             return measureText(this.txt).height;
         }
+        if (this.img.trimmedTileHeight) {
+            return this.img.trimmedTileHeight;
+        }
         if (this.img.scale)
-            return Math.floor(this.img.height * this.img.scale);
+            return this.img.height * this.img.scale;
         return this.img.height;
     };
 
@@ -49,8 +53,11 @@ var Drawable = (function (Math, measureText) {
         if (this.txt) {
             return measureText(this.txt).width;
         }
+        if (this.img.trimmedTileWidth) {
+            return this.img.trimmedTileWidth;
+        }
         if (this.img.scale)
-            return Math.floor(this.img.width * this.img.scale);
+            return this.img.width * this.img.scale;
         return this.img.width;
     };
 
