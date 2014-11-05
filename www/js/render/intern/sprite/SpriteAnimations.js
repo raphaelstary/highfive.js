70699c10cc11b3ac65c56c9a8644571415ac07ec
Raphael Stary <raphael.stary@gmail.com>
2014-11-05 11:23:22 +0100

    adapted class architecture to other animation and motion classes #core (cherry picked from commit 93fc11d)

--- www/js/render/intern/sprite/SpriteAnimations.js
+++ www/js/render/intern/sprite/SpriteAnimations.js
@@ -10,37 +10,31 @@ var SpriteAnimations = (function (Object) {
         drawable.data = sprite.frames[0];
 
         this.animationsDict[drawable.id] = {
-            item: drawable,
+            drawable: drawable,
             sprite: sprite,
             ready: callback,
             time: 0
         };
     };
 
-    SpriteAnimations.prototype.remove = function (drawable) {
-        delete this.animationsDict[drawable.id];
-    };
-
-    SpriteAnimations.prototype.has = function (drawable) {
-        return this.animationsDict[drawable.id] !== undefined;
-    };
-
     SpriteAnimations.prototype.nextFrame = function () {
         Object.keys(this.animationsDict).forEach(function (key) {
             var animation = this.animationsDict[key];
-            var item = animation.item;
+            var drawable = animation.drawable;
             var sprite = animation.sprite;
 
-            item.data = sprite.frames[++animation.time];
+            drawable.data = sprite.frames[++animation.time];
             if (animation.time >= sprite.frames.length) {
-                if (this.animationsDict[key].ready !== undefined) {
-                    this.animationsDict[key].ready();
-                }
+
                 if (sprite.loop) {
                     animation.time = 0;
-                    item.data = sprite.frames[0];
+                    drawable.data = sprite.frames[0];
                 } else {
                     delete this.animationsDict[key];
+                }
+
+                if (animation.ready) {
+                    animation.ready();
                 }
             }
         }, this);
@@ -52,6 +46,14 @@ var SpriteAnimations = (function (Object) {
             this.ticker = 0;
         }
         this.ticker++;
+    };
+
+    SpriteAnimations.prototype.remove = function (drawable) {
+        delete this.animationsDict[drawable.id];
+    };
+
+    SpriteAnimations.prototype.has = function (drawable) {
+        return this.animationsDict[drawable.id] !== undefined;
     };
 
     return SpriteAnimations;
