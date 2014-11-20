60fdbd2ad757e89c2abd498755cfe7b657abc879
Raphael Stary <raphael.stary@gmail.com>
2014-11-20 23:22:33 +0100

    reformatting resolve atlas function (cherry picked from commit dcdec2f)

--- www/js/net/resolveAtlasPaths.js
+++ www/js/net/resolveAtlasPaths.js
@@ -2,32 +2,85 @@ var resolveAtlasPaths = (function () {
     "use strict";
 
     var atlases = [
-        {size: 480, count: 1},
-        {size: 600, count: 2},
-        {size: 614, count: 2},
-        {size: 720, count: 3},
-        {size: 768, count: 3},
-        {size: 800, count: 3},
-        {size: 864, count: 3},
-        {size: 900, count: 4},
-        {size: 960, count: 4},
-        {size: 1024, count: 5},
-        {size: 1050, count: 5},
-        {size: 1080, count: 5},
-        {size: 1136, count: 6},
-        {size: 1152, count: 6},
-        {size: 1200, count: 6},
-        {size: 1280, count: 7},
-        {size: 1366, count: 8},
-        {size: 1440, count: 9},
-        {size: 1600, count: 12},
-        {size: 1716, count: 13},
-        {size: 1920, count: 16},
-        {size: 2048, count: 22},
-        {size: 2160, count: 1},
-        {size: 2560, count: 1},
-        {size: 3200, count: 1},
-        {size: 3840, count: 2}
+        {
+            size: 480,
+            count: 1
+        }, {
+            size: 600,
+            count: 1
+        }, {
+            size: 614,
+            count: 1
+        }, {
+            size: 720,
+            count: 1
+        }, {
+            size: 768,
+            count: 1
+        }, {
+            size: 800,
+            count: 1
+        }, {
+            size: 864,
+            count: 1
+        }, {
+            size: 900,
+            count: 1
+        }, {
+            size: 960,
+            count: 1
+        }, {
+            size: 1024,
+            count: 1
+        }, {
+            size: 1050,
+            count: 1
+        }, {
+            size: 1080,
+            count: 1
+        }, {
+            size: 1136,
+            count: 1
+        }, {
+            size: 1152,
+            count: 1
+        }, {
+            size: 1200,
+            count: 1
+        }, {
+            size: 1280,
+            count: 1
+        }, {
+            size: 1366,
+            count: 1
+        }, {
+            size: 1440,
+            count: 1
+        }, {
+            size: 1600,
+            count: 1
+        }, {
+            size: 1716,
+            count: 2
+        }, {
+            size: 1920,
+            count: 2
+        }, {
+            size: 2048,
+            count: 2
+        }, {
+            size: 2160,
+            count: 1
+        }, {
+            size: 2560,
+            count: 1
+        }, {
+            size: 3200,
+            count: 1
+        }, {
+            size: 3840,
+            count: 1
+        }
     ];
 
     var ATLAS = 'atlas';
