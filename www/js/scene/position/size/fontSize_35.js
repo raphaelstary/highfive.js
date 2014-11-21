5e24912c9670230413dbc598a6d9ca9abf154845
Raphael Stary <raphael.stary@gmail.com>
2014-11-21 10:29:00 +0100

    new font size functions #core (cherry picked from commit 4a4b032)

---
+++ www/js/scene/position/size/fontSize_35.js
@@ -0,0 +1,7 @@
+var fontSize_35 = (function (calcScreenConst) {
+    "use strict";
+
+    return function (width, height) {
+        return calcScreenConst(height, 35);
+    };
+})(calcScreenConst);
---
+++ www/js/scene/position/size/fontSize_40.js
@@ -0,0 +1,7 @@
+var fontSize_40 = (function (calcScreenConst) {
+    "use strict";
+
+    return function (width, height) {
+        return calcScreenConst(height, 40);
+    };
+})(calcScreenConst);
