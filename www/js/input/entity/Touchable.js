5a6357c887bfd034f1e4e4dcd88bc45ce5215eaa
raphaelstary <raphael.stary@gmail.com>
2014-11-26 16:10:50 +0100

    just a #code todo (cherry picked from commit eec0ccd)

--- www/js/input/entity/Touchable.js
+++ www/js/input/entity/Touchable.js
@@ -2,6 +2,7 @@ var Touchable = (function () {
     "use strict";
 
     function Touchable(id, x, y, width, height) {
+        // todo: change them (touchables) to AB rects, cause I think it relates more to the domain
         this.id = id;
         this.x = x;
         this.y = y;
