6731c95fc89d915f56ea5e6f527407400089baca
raphaelstary <raphael.stary@gmail.com>
2014-11-28 17:55:24 +0100

    fix scope bug #core (cherry picked from commit b2eeae3)

--- www/js/utils/iterateSomeEntries.js
+++ www/js/utils/iterateSomeEntries.js
@@ -2,9 +2,8 @@ var iterateSomeEntries = (function () {
     "use strict";
 
     function iterateSomeEntries(dictionary, callback, self) {
-        callback.bind(self);
         Object.keys(dictionary).some(function (key) {
-            return callback(dictionary[key]);
+            return callback.call(self, dictionary[key]);
         });
     }
 
