1a0ee5da02eeb3ef3f3b1a50849cd58d0626f249
raphaelstary <raphael.stary@gmail.com>
2014-12-03 12:01:48 +0100

    add mobile check #core (cherry picked from commit 7170b02)

--- www/js/platform/BrowserOracle.js
+++ www/js/platform/BrowserOracle.js
@@ -3,11 +3,9 @@ var BrowserOracle = (function () {
 
     function BrowserOracle(userAgent) {
         this.userAgent = userAgent;
-    }
-
-    BrowserOracle.prototype.init = function () {
         this.isFirefox = /firefox/i.test(this.userAgent);
-    };
+        this.isMobile = /mobile/i.test(this.userAgent);
+    }
 
     return BrowserOracle;
 })();
