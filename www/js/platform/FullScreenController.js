182dcaa97018f46415e63f23f03f059b16e35d35
raphaelstary <raphael.stary@gmail.com>
2014-12-11 11:17:43 +0100

    better full screen controller refs #35 - handle orientation #core (cherry picked from commit 79779cf)

--- www/js/platform/FullScreenController.js
+++ www/js/platform/FullScreenController.js
@@ -1,27 +1,60 @@
-var FullScreenController = (function () {
+var FullScreenController = (function (document) {
     "use strict";
 
     function FullScreenController(screen) {
-        this.requestFullscreen = (function (element) {
-            if (element.requestFullscreen) {
-                return element.requestFullscreen.bind(element);
-            } else if (element.webkitRequestFullscreen) {
-                return element.webkitRequestFullscreen.bind(element);
-            } else if (element.mozRequestFullScreen) {
-                return element.mozRequestFullScreen.bind(element);
-            } else if (element.msRequestFullscreen) {
-                return element.msRequestFullscreen.bind(element);
-            }
-            return null;
-        })(screen);
+        this.screen = screen;
 
-        this.isSupported = this.requestFullscreen != null;
+        this.isSupported = document.fullscreenEnabled || document.webkitFullscreenEnabled ||
+        document.mozFullScreenEnabled || document.msFullscreenEnabled;
     }
 
     FullScreenController.prototype.request = function () {
-        if (this.isSupported)
-            this.requestFullscreen();
+        if (this.isSupported) {
+            if (this.screen.requestFullscreen) {
+                this.screen.requestFullscreen();
+            } else if (this.screen.webkitRequestFullscreen) {
+                this.screen.webkitRequestFullscreen();
+            } else if (this.screen.mozRequestFullScreen) {
+                this.screen.mozRequestFullScreen();
+            } else if (this.screen.msRequestFullscreen) {
+                this.screen.msRequestFullscreen();
+            }
+        }
+        return this.isFullScreen();
     };
 
+    FullScreenController.prototype.isFullScreen = function () {
+        return document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement ||
+            document.msFullscreenElement;
+    };
+
+    FullScreenController.prototype.exit = function () {
+        if (this.isSupported) {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+        }
+
+        return !this.isFullScreen();
+    };
+
+    document.addEventListener("fullscreenchange", fullScreenDebugHandler);
+    document.addEventListener("webkitfullscreenchange", fullScreenDebugHandler);
+    document.addEventListener("mozfullscreenchange", fullScreenDebugHandler);
+    document.addEventListener("MSFullscreenChange", fullScreenDebugHandler);
+
+    function fullScreenDebugHandler() {
+        console.log('full screen event fired');
+        var isFs = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement ||
+            document.msFullscreenElement;
+        console.log('is full screen: ' + isFs);
+    }
+
     return FullScreenController;
-})();
+})(window.document);
