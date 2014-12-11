8a66e07c9cee47bad1fd71c1bc8db3056f974dd2
raphaelstary <raphael.stary@gmail.com>
2014-12-11 17:48:45 +0100

    ie11 can't handle full screen requests from events other than type click refs #35 - handle orientation #core (cherry picked from commit 1693319)

--- www/js/input/bootstrap/installOneTimeTap.js
+++ www/js/input/bootstrap/installOneTimeTap.js
@@ -2,35 +2,19 @@ var installOneTimeTap = (function (window) {
     "use strict";
 
     function installOneTimeTap(element, callback) {
-        if (window.PointerEvent) {
-            element.addEventListener('pointerdown', handleClick);
-
-        } else if (window.MSPointerEvent) {
-            element.addEventListener('MSPointerDown', handleClick);
-
-        } else {
-            if ('ontouchstart' in window) {
-                element.addEventListener('touchstart', handleClick);
-            }
-
-            element.addEventListener('click', handleClick);
+        if ('ontouchstart' in window) {
+            element.addEventListener('touchstart', handleClick);
         }
+
+        element.addEventListener('click', handleClick);
         function handleClick(event) {
             event.preventDefault();
 
-            if (window.PointerEvent) {
-                element.removeEventListener('pointerdown', handleClick);
-
-            } else if (window.MSPointerEvent) {
-                element.removeEventListener('MSPointerDown', handleClick);
-
-            } else {
-                if ('ontouchstart' in window) {
-                    element.removeEventListener('touchstart', handleClick);
-                }
-
-                element.removeEventListener('click', handleClick);
+            if ('ontouchstart' in window) {
+                element.removeEventListener('touchstart', handleClick);
             }
+
+            element.removeEventListener('click', handleClick);
 
             callback();
         }
