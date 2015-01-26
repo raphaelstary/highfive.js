d3d35be0293dd5c5fadd6878c8289b7781dc29f8
raphaelstary <raphael.stary@gmail.com>
2015-01-26 17:41:48 +0100

    movement plus shooting refs #2 #core (cherry picked from commit 9162266)

--- www/js/highfivejs/input/entity/GamePad.js
+++ www/js/highfivejs/input/entity/GamePad.js
@@ -8,24 +8,24 @@ var GamePad = (function (navigator) {
         Y: 3,
         LEFT_BUMPER: 4,
         RIGHT_BUMPER: 5,
-        BACK: 6,
-        START: 7,
-        GUIDE: 8,
-        LEFT_STICK: 9,
-        RIGHT_STICK: 10,
-        D_PAD_LEFT: 11,
-        D_PAD_RIGHT: 12,
-        D_PAD_UP: 13,
-        D_PAD_DOWN: 14
+        LEFT_TRIGGER: 6,
+        RIGHT_TRIGGER: 7,
+        BACK: 8,
+        START: 9,
+        LEFT_STICK: 10,
+        RIGHT_STICK: 11,
+        D_PAD_UP: 12,
+        D_PAD_DOWN: 13,
+        D_PAD_LEFT: 14,
+        D_PAD_RIGHT: 15,
+        GUIDE: 16
     };
 
     var Axis = {
         LEFT_STICK_X: 0,
         LEFT_STICK_Y: 1,
-        LEFT_TRIGGER: 2,
-        RIGHT_STICK_X: 3,
-        RIGHT_STICK_Y: 4,
-        RIGHT_TRIGGER: 5
+        RIGHT_STICK_X: 2,
+        RIGHT_STICK_Y: 3
     };
 
     function GamePad(index) {
@@ -64,27 +64,27 @@ var GamePad = (function (navigator) {
         return this.axes[Axis.RIGHT_STICK_Y];
     };
 
-    GamePad.prototype.getRightTrigger = function () {
-        return this.axes[Axis.RIGHT_TRIGGER];
+    GamePad.prototype.isRightTriggerPressed = function () {
+        return this.__isButtonPressed(Button.RIGHT_TRIGGER);
     };
 
     GamePad.prototype.isAPressed = function () {
-        return this._isButtonPressed(Button.A);
+        return this.__isButtonPressed(Button.A);
     };
 
     GamePad.prototype.isBPressed = function () {
-        return this._isButtonPressed(Button.B);
+        return this.__isButtonPressed(Button.B);
     };
 
     GamePad.prototype.isRightBumperPressed = function () {
-        return this._isButtonPressed(Button.RIGHT_BUMPER);
+        return this.__isButtonPressed(Button.RIGHT_BUMPER);
     };
 
     GamePad.prototype.isLeftBumperPressed = function () {
-        return this._isButtonPressed(Button.LEFT_BUMPER);
+        return this.__isButtonPressed(Button.LEFT_BUMPER);
     };
 
-    GamePad.prototype._isButtonPressed = function (button) {
+    GamePad.prototype.__isButtonPressed = function (button) {
         return this.buttons[button].pressed;
     };
 
