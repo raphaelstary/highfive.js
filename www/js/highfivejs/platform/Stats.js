2f02afd7f7d0fc6d742c1bbc86631f46de6ff84b
raphaelstary <raphael.stary@gmail.com>
2015-02-19 17:36:32 +0100

    introduce a fps meter refs #29 #core (cherry picked from commit 0dc011f)

---
+++ www/js/highfivejs/platform/Stats.js
@@ -0,0 +1,46 @@
+var Stats = (function (Math) {
+    "use strict";
+    var startTime = Date.now(), previousTime = startTime;
+    var ms = 0, msMin = Infinity, msMax = 0;
+    var fps = 0, fpsMin = Infinity, fpsMax = 0;
+    var frames = 0;
+
+    return {
+        getFps: function () {
+            return fps;
+        },
+        getMs: function () {
+            return ms;
+        },
+        start: function () {
+            startTime = Date.now();
+        },
+        end: function () {
+            var time = Date.now();
+
+            ms = time - startTime;
+            msMin = Math.min(msMin, ms);
+            msMax = Math.max(msMax, ms);
+
+            //this.data.ms = ms;
+            //this.data.msMin = msMin;
+            //this.data.msMax = msMax;
+
+            frames++;
+
+            if (time > previousTime + 1000) {
+
+                fps = Math.round(( frames * 1000 ) / ( time - previousTime ));
+                fpsMin = Math.min(fpsMin, fps);
+                fpsMax = Math.max(fpsMax, fps);
+
+                //this.data.fps = fps;
+                //this.data.fpsMin = fpsMin;
+                //this.data.fpsMax = fpsMax;
+
+                previousTime = time;
+                frames = 0;
+            }
+        }
+    };
+})(Math);
