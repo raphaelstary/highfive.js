60c0b6e9c78a9a05c2710002b13b19691f928f07
raphaelstary <raphael.stary@gmail.com>
2015-03-07 18:47:35 +0100

    local storage wrapper refs #69 #core (cherry picked from commit 2b2e371)

---
+++ www/js/highfivejs/platform/lclStorage.js
@@ -0,0 +1,20 @@
+var lclStorage = (function (window) {
+    "use strict";
+
+    var lclStorage;
+    try {
+        lclStorage = window.localStorage;
+    } catch (e) {
+        lclStorage = {
+            dict: {},
+            getItem: function (id) {
+                return this.dict[id];
+            },
+            setItem: function (id, value) {
+                this.dict[id] = value;
+            }
+        }
+    }
+
+    return lclStorage;
+})(window);
