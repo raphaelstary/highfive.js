92cf33463f234e09e9ef00ff070fa312d8d16051
raphaelstary <raphael.stary@gmail.com>
2015-05-15 15:26:43 +0200

    return value for some function #core (cherry picked from commit 178f547)

--- www/js/highfivejs/utils/iterateSomeEntries.js
+++ www/js/highfivejs/utils/iterateSomeEntries.js
@@ -2,7 +2,7 @@ var iterateSomeEntries = (function () {
     "use strict";
 
     function iterateSomeEntries(dictionary, callback, self) {
-        Object.keys(dictionary).some(function (key) {
+        return Object.keys(dictionary).some(function (key) {
             return callback.call(self, dictionary[key]);
         });
     }
