58c1fb86eb905966e2df7b97f428fbba0520fada
raphaelstary <raphael.stary@gmail.com>
2015-08-12 18:37:26 +0200

    rendering basic scene with touch callbacks #core (cherry picked from commit 8e18778)

--- www/js/highfivejs/input/entity/Touchable.js
+++ www/js/highfivejs/input/entity/Touchable.js
@@ -3,6 +3,8 @@ var Touchable = (function () {
 
     function Touchable(id, x, y, width, height) {
         // todo: change them (touchables) to AB rects, cause I think it relates more to the domain
+        // todo: new idea: change em to drawables because it's a lot easier to work with!
+        // (share same object.. resize stuff.. link stuff.. no context switch in your head ...)
         this.id = id;
         this.x = x;
         this.y = y;
--- www/js/highfivejs/scene/MVVMScene.js
+++ www/js/highfivejs/scene/MVVMScene.js
@@ -17,8 +17,6 @@ var MVVMScene = (function (iterateEntries, Width, Height) {
 
         this.view = view;
         this.viewModel = viewModel;
-
-        this.dict = {};
     }
 
     MVVMScene.prototype.show = function (next) {
@@ -69,7 +67,7 @@ var MVVMScene = (function (iterateEntries, Width, Height) {
                         elem.scale);
                     drawables.push(drawable);
                     if (elem.viewId) {
-                        this.dict[elem.viewId] = drawable;
+                        this.viewModel[elem.viewId] = drawable;
                     }
 
                 } else if (elem.type == 'text') {
@@ -77,33 +75,112 @@ var MVVMScene = (function (iterateEntries, Width, Height) {
                         undefined, elem.rotation, elem.alpha, undefined, undefined, elem.scale);
                     drawables.push(drawable);
                     if (elem.viewId) {
-                        this.dict[elem.viewId] = drawable;
+                        this.viewModel[elem.viewId] = drawable;
                     }
 
                 } else if (elem.type == 'rectangle') {
-                    drawable = this.stage.drawRectangle(x, y, xFn(elem.width), yFn(elem.height), elem.color,
-                        elem.filled, undefined, zIndex, elem.alpha, elem.rotation, elem.scale, undefined);
-                    drawables.push(drawable);
-                    if (elem.viewId) {
-                        this.dict[elem.viewId] = drawable;
+                    var isInput = elem.tags.some(function (tag) {
+                        return tag == 'input';
+                    });
+                    if (isInput) {
+                        var fnName;
+                        elem.tags.some(function (tag) {
+                            var foundSmth = tag.tap !== undefined;
+                            if (foundSmth)
+                                fnName = tag.tap;
+                            return foundSmth;
+                        });
+                        var wrapper = this.stage.drawRectangleWithInput(x, y, xFn(elem.width), yFn(elem.height),
+                            '#fff');
+                        this.tap.add(wrapper.input, this.viewModel[fnName].bind(this.viewModel));
+
+                        this.stage.hide(wrapper.drawable); //todo: clean up this mess and stop this drawable/input shit
+                        drawables.push(wrapper.drawable);
+                        taps.push(wrapper.input);
+
+                    } else {
+                        drawable = this.stage.drawRectangle(x, y, xFn(elem.width), yFn(elem.height), elem.color,
+                            elem.filled, undefined, zIndex, elem.alpha, elem.rotation, elem.scale, undefined);
+                        drawables.push(drawable);
+
+                        if (elem.viewId) {
+                            this.viewModel[elem.viewId] = drawable;
+                        }
                     }
 
                 } else if (elem.type == 'button') {
-                    x = xFn(elem.text.x);
-                    y = yFn(elem.text.y);
+                    //x = xFn(elem.text.x);
+                    //y = yFn(elem.text.y);
 
-                    drawable = this.buttons.createPrimaryButton(x, y, elem.text.msg, undefined, zIndex, undefined,
-                        xFn(elem.background.width), yFn(elem.background.height));
-                    // todo: rethink this shit with buttons
+                    //var callbackFnName;
+                    //elem.input.tags.some(function (tag) {
+                    //    var foundSmth = tag.tap !== undefined;
+                    //    if (foundSmth)
+                    //        callbackFnName = tag.tap;
+                    //    return foundSmth;
+                    //});
 
-                    buttons.push(drawable);
-                    if (elem.viewId) {
-                        this.dict[elem.viewId] = drawable;
+                    //drawable = this.buttons.createPrimaryButton(x, y, elem.text.msg,
+                    //    this.viewModel[callbackFnName].bind(this.viewModel), zIndex, false,
+                    // xFn(elem.background.width), yFn(elem.background.height)); //todo: rethink this shit with buttons
+                    //   buttons.push(drawable);  if (elem.viewId) {  this.viewModel[elem.viewId] = drawable;  }
+
+                    var btnFnName;
+                    elem.input.tags.some(function (tag) {
+                        var foundSmth = tag.tap !== undefined;
+                        if (foundSmth)
+                            btnFnName = tag.tap;
+                        return foundSmth;
+                    });
+                    var inputWrapper = this.stage.drawRectangleWithInput(xFn(elem.input.x), yFn(elem.input.y),
+                        xFn(elem.input.width), yFn(elem.input.height), '#fff');
+                    this.tap.add(inputWrapper.input, this.viewModel[btnFnName].bind(this.viewModel));
+
+                    this.stage.hide(inputWrapper.drawable); //todo: clean up this mess and stop this drawable/input shit
+                    drawables.push(inputWrapper.drawable);
+                    taps.push(inputWrapper.input);
+
+                    drawable = this.stage.drawText(xFn(elem.text.x), yFn(elem.text.y), elem.text.msg,
+                        yFn(elem.text.size), elem.text.font, elem.text.color, zIndex + 1, undefined, elem.text.rotation,
+                        elem.text.alpha, undefined, undefined, elem.text.scale);
+                    drawables.push(drawable);
+
+                    if (elem.background.type == 'image') {
+                        var bgName = elem.background.filename.substring(0, elem.background.filename.lastIndexOf('.'));
+                        drawable = this.stage.drawFresh(xFn(elem.background.x), yFn(elem.background.y), bgName, zIndex,
+                            undefined, elem.background.alpha, elem.background.rotation, elem.background.scale);
+                        drawables.push(drawable);
+
+                    } else if (elem.background.type == 'rectangle') {
+                        drawable = this.stage.drawRectangle(xFn(elem.background.x), yFn(elem.background.y),
+                            xFn(elem.background.width), yFn(elem.background.height), elem.background.color,
+                            elem.background.filled, undefined, zIndex, elem.background.alpha, elem.background.rotation,
+                            elem.background.scale, undefined);
+                        drawables.push(drawable);
                     }
+
                 }
 
             }, this);
         }, this);
+
+        if (this.viewModel.postConstruct)
+            this.viewModel.postConstruct();
+
+        var self = this;
+
+        function nextScene() {
+            if (self.viewModel.preDestroy)
+                self.viewModel.preDestroy();
+
+            drawables.forEach(self.stage.remove.bind(self.stage));
+            buttons.forEach(self.buttons.remove.bind(self.buttons));
+            taps.forEach(self.tap.remove.bind(self.tap));
+
+            next();
+        }
+
+        this.viewModel.nextScene = nextScene;
     };
 
     return MVVMScene;
