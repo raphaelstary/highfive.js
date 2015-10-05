40a01724cf99e12b7353c74d4993bee783a29213
raphaelstary <raphael.stary@gmail.com>
2015-10-05 13:16:08 +0200

    callback counter #core (cherry picked from commit bb71dbc)

---
+++ www/js/highfivejs/utils/CallbackCounter.js
@@ -0,0 +1,26 @@
+var CallbackCounter = (function () {
+    "use strict";
+
+    function CallbackCounter(callback) {
+        this.counter = 0;
+        this.callback = callback;
+    }
+
+    CallbackCounter.prototype.registerCallback = function () {
+        this.counter++;
+        return this.__onProgress.bind(this);
+    };
+
+    CallbackCounter.prototype.__onProgress = function () {
+        if (--this.counter === 0)
+            this.__onComplete();
+    };
+
+    CallbackCounter.prototype.__onComplete = function () {
+        this.counter = 0;
+        if (this.callback)
+            this.callback();
+    };
+
+    return CallbackCounter;
+})();
