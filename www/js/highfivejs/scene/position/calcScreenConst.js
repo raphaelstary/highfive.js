efa073335f6ef3685231f51bfaaf1d6fca006c75
raphaelstary <raphael.stary@gmail.com>
2016-08-02 19:32:22 +0200

    fix issue with falsy interpretation of 0 #core (cherry picked from commit bf20b7b)

--- www/js/highfivejs/scene/position/calcScreenConst.js
+++ www/js/highfivejs/scene/position/calcScreenConst.js
@@ -2,7 +2,7 @@ H5.calcScreenConst = (function (Math) {
     "use strict";
 
     function calcScreenConst(domain, denominator, numerator) {
-        return Math.floor(domain / denominator * (numerator || 1));
+        return Math.floor(domain / denominator * (numerator !== undefined ? numerator : 1));
     }
 
     return calcScreenConst;
