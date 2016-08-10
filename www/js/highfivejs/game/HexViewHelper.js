41760b6cf8acd07da9b945725106f282cd282c8f
raphaelstary <raphael.stary@gmail.com>
2016-08-10 15:56:01 +0200

    introduce offset to hex view grid helper #core (cherry picked from commit 78d6eea)

--- www/js/highfivejs/game/HexViewHelper.js
+++ www/js/highfivejs/game/HexViewHelper.js
@@ -1,10 +1,12 @@
 H5.HexViewHelper = (function (Width, Math) {
     "use strict";
 
-    function HexViewHelper(stage, xTilesCount, yTilesCount) {
+    function HexViewHelper(stage, xTilesCount, yTilesCount, xOffset, yOffset) {
         this.stage = stage;
         this.xTiles = xTilesCount;
         this.yTiles = yTilesCount;
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
     }
 
     var POINTY_TOPPED_ANGLE = Math.PI / 6;
@@ -19,17 +21,14 @@ H5.HexViewHelper = (function (Width, Math) {
     };
 
     HexViewHelper.prototype.getWidth = function (width) {
-        return Width.get(this.xTiles + 0.5)(width);
-    };
-
-    HexViewHelper.prototype.getWidthHalf = function (width) {
-        return Math.floor(this.getWidth(width) / 2);
+        return Width.get(this.xTiles + 0.5)(width - this.xOffset(width));
     };
 
     HexViewHelper.prototype.getXFn = function (u, v) {
         return (function (self) {
             return function (width) {
-                return Math.floor(self.getSize(width) * Math.sqrt(3) * (u + 0.5 * (v & 1)) + self.getWidthHalf(width));
+                return Math.floor(self.getSize(width) * Math.sqrt(3) * (u + 0.5 * (v & 1)) + self.getWidth(width) / 2 +
+                    self.xOffset(width));
             };
         })(this);
     };
@@ -37,7 +36,7 @@ H5.HexViewHelper = (function (Width, Math) {
     HexViewHelper.prototype.getYFn = function (v) {
         return (function (self) {
             return function (height, width) {
-                return Math.floor(self.getSize(width) * 3 / 2 * v + self.getSize(width));
+                return Math.floor(self.getSize(width) * 3 / 2 * v + self.getSize(width) + self.yOffset(height));
             };
         })(this);
     };
