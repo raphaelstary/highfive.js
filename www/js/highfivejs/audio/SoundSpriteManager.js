d3b0d94fe3ceb28cd47bd519c387d03b349adaf1
raphaelstary <raphael.stary@gmail.com>
2016-08-25 12:41:45 +0200

    add sounds to the game #core (cherry picked from commit 62a5a5e)

--- www/js/highfivejs/audio/SoundSpriteManager.js
+++ www/js/highfivejs/audio/SoundSpriteManager.js
@@ -10,8 +10,32 @@ H5.SoundSpriteManager = (function (Howl) {
     };
 
     SoundSpriteManager.prototype.play = function (name) {
-        this.sounds.play(name);
+        return this.sounds.play(name);
+    };
+
+    SoundSpriteManager.prototype.loop = function (id) {
+        this.sounds.loop(true, id);
+    };
+
+    SoundSpriteManager.prototype.stop = function (id) {
+        this.sounds.stop(id);
+    };
+
+    SoundSpriteManager.prototype.subscribe = function (id, event, callback) {
+        this.sounds.on(event, callback, id);
+    };
+
+    SoundSpriteManager.prototype.notifyOnce = function (id, event, callback) {
+        this.sounds.once(event, callback, id);
+    };
+
+    SoundSpriteManager.prototype.fadeOut = function (id) {
+        this.sounds.fade(1, 0, 500, id);
+    };
+
+    SoundSpriteManager.prototype.isPlaying = function (id) {
+        return this.sounds.playing(id);
     };
 
     return SoundSpriteManager;
-})(function(){});
+})(Howl);
