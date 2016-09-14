c6a3e0f55ecf3803bb2f9ac7c823569284282b12
raphaelstary <raphael.stary@gmail.com>
2016-09-14 17:15:53 +0200

    scale just rect's width or just height #core (cherry picked from commit 9a66c3c)

--- www/js/highfivejs/render/intern/draw/renderRectangle.js
+++ www/js/highfivejs/render/intern/draw/renderRectangle.js
@@ -1,41 +1,35 @@
-H5.renderRectangle = (function () {
+H5.renderRectangle = (function (Math) {
     "use strict";
 
     function renderRectangle(ctx, drawable) {
+        var x = drawable.justHeightScale ? drawable.x - Math.floor(drawable.__getWidth() / 2) - 0.5 :
+        drawable.getCornerX() - 0.5;
+        var y = drawable.justWidthScale ? drawable.y - Math.floor(drawable.__getHeight() / 2) - 0.5 :
+        drawable.getCornerY() - 0.5;
+        var width = drawable.justHeightScale ? Math.floor(drawable.__getWidth()) : drawable.getWidth();
+        var height = drawable.justWidthScale ? Math.floor(drawable.__getHeight()) : drawable.getHeight();
+
         if (drawable.data.filled) {
             ctx.fillStyle = drawable.data.color;
             if (drawable.flipHorizontally) {
-                ctx.fillRect((drawable.getCornerX() - 0.5) * -1, drawable.getCornerY() - 0.5, drawable.getWidth(),
-                    drawable.getHeight());
+                ctx.fillRect(x * -1, y, width, height);
             } else {
-                ctx.fillRect(drawable.getCornerX() - 0.5, drawable.getCornerY() - 0.5, drawable.getWidth(),
-                    drawable.getHeight());
+                ctx.fillRect(x, y, width, height);
             }
-        } else if (drawable.justHeightScale) {
-            ctx.strokeStyle = drawable.data.color;
-            if (drawable.data.lineWidth)
-                ctx.lineWidth = drawable.data.lineWidth;
-
-            ctx.strokeRect(drawable.x - Math.floor(drawable.__getWidth() / 2) - 0.5, drawable.getCornerY() - 0.5,
-                Math.floor(drawable.__getWidth()), drawable.getHeight());
-
         } else {
-            if (drawable.data.lineDash !== undefined) {
+            if (drawable.data.lineDash !== undefined)
                 ctx.setLineDash(drawable.data.lineDash);
-            }
 
             ctx.strokeStyle = drawable.data.color;
             if (drawable.data.lineWidth)
                 ctx.lineWidth = drawable.data.lineWidth;
             if (drawable.flipHorizontally) {
-                ctx.strokeRect((drawable.getCornerX() - 0.5) * -1, drawable.getCornerY() - 0.5, drawable.getWidth(),
-                    drawable.getHeight());
+                ctx.strokeRect(x * -1, y, width, height);
             } else {
-                ctx.strokeRect(drawable.getCornerX() - 0.5, drawable.getCornerY() - 0.5, drawable.getWidth(),
-                    drawable.getHeight());
+                ctx.strokeRect(x, y, width, height);
             }
         }
     }
 
     return renderRectangle;
-})();
+})(Math);
