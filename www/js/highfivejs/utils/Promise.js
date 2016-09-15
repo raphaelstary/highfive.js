89f78e6b77101e8c84bf3897bae00f41f15c54fc
raphaelstary <raphael.stary@gmail.com>
2016-09-15 15:52:06 +0200

    h5.js promise impl #core (cherry picked from commit 118cc89)

---
+++ www/js/highfivejs/utils/Promise.js
@@ -0,0 +1,29 @@
+H5.Promise = (function () {
+    "use strict";
+
+    function Promise() {
+        this.isFulfilled = false;
+    }
+
+    Promise.prototype.then = function (callback) {
+        this.__callback = callback;
+
+        if (this.isFulfilled)
+            callback(this.__arg);
+    };
+
+    Promise.prototype.resolve = function (arg) {
+        if (this.isFulfilled)
+            return;
+
+        this.isFulfilled = true;
+
+        if (this.__callback) {
+            this.__callback(arg);
+        } else {
+            this.__arg = arg;
+        }
+    };
+
+    return Promise;
+})();
