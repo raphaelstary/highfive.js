caac664d6dd6746eb059020cd50203be1cd8ec19
raphaelstary <raphael.stary@gmail.com>
2016-11-29 19:20:57 +0100

    introduces selection list - a custom ui component to select and deselect items like an accordion #core
    (cherry picked from commit 82c3b09)

---
+++ www/js/highfivejs/scene/component/SelectionList.js
@@ -0,0 +1,43 @@
+H5.SelectionList = (function () {
+    "use strict";
+
+    function SelectionList() {
+        this.options = [];
+        this.selection = null;
+    }
+
+    SelectionList.prototype.add = function (drawable, callback, self) {
+        var item = {
+            selection: drawable,
+            callback: self ? callback.bind(self) : callback
+        };
+
+        if (this.options.length == 0) this.selection = item.callback;
+        if (this.options.length != 0) drawable.setShow(false);
+
+        this.options.push(item);
+    };
+
+    SelectionList.prototype.previous = function () {
+        var newSelection = this.options.pop();
+        newSelection.selection.show = true;
+        this.options[0].selection.show = false;
+        this.options.unshift(newSelection);
+
+        this.selection = newSelection.callback;
+    };
+
+    SelectionList.prototype.next = function () {
+        this.options.push(this.options.shift());
+        this.options[0].selection.show = true;
+        this.options[this.options.length - 1].selection.show = false;
+
+        this.selection = this.options[0].callback;
+    };
+
+    SelectionList.prototype.execute = function () {
+        this.selection();
+    };
+
+    return SelectionList;
+})();
