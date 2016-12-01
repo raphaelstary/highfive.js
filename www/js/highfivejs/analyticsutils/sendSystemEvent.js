a68889b5ebcc355ff40fd29b7c629d7480c03e67
raphaelstary <raphael.stary@gmail.com>
2016-12-01 05:48:15 -0800

    add position & game pad info to the system-analytics event helper #core
    (cherry picked from commit d23d21c)

--- www/js/highfivejs/analyticsutils/sendSystemEvent.js
+++ www/js/highfivejs/analyticsutils/sendSystemEvent.js
@@ -1,28 +1,72 @@
-H5.sendSystemEvent = (function (Event, location, appName, appVersion) {
+H5.sendSystemEvent = (function (Event, location, appName, appVersion, geolocation, Promise, GamePad) {
     "use strict";
 
-    function sendSystemEvent(device, messages, events) {
-        var payload = {
-            type: 'system',
-            width: device.width,
-            height: device.height,
-            cssWidth: device.cssWidth,
-            cssHeight: device.cssHeight,
-            screenWidth: device.screenWidth,
-            screenHeight: device.screenHeight,
-            devicePixelRatio: device.devicePixelRatio,
-            mobile: device.isMobile,
-            userAgent: device.userAgent,
-            language: messages.defaultLanguageCode,
-            userTime: Date.now(),
-            userTimeString: Date(),
-            location: location.href,
-            app: appName,
-            version: appVersion
-        };
+    function collectPositionInfo() {
+        var promise = new Promise();
 
-        events.fire(Event.ANALYTICS, payload);
+        geolocation.getCurrentPosition(function (position) {
+            promise.resolve(position);
+        }, function (error) {
+            promise.resolve(error);
+        }, {enableHighAccuracy: false});
+
+        return promise;
+    }
+
+    function collectGamePadInfo() {
+        var gamePad = new GamePad();
+        var gamePads = gamePad.getGamePads();
+        var padsInfo = [];
+        var connectedGamePads = 0;
+        for (var i = 0; i < gamePads.length; i++) {
+            var currentPad = gamePads[i];
+            if (currentPad) {
+                connectedGamePads++;
+
+                padsInfo.push({
+                    id: gamePads[i].id,
+                    index: gamePads[i].index,
+                    mapping: gamePads[i].mapping,
+                    profile: gamePads[i].profile
+                });
+            }
+        }
+
+        return {
+            connectedGamePads: connectedGamePads,
+            gamePadInfo: padsInfo
+        }
+    }
+
+    function sendSystemEvent(device, messages, events) {
+
+        collectPositionInfo().then(function (positionInfo) {
+
+            //noinspection JSPotentiallyInvalidConstructorUsage
+            var payload = {
+                type: 'system',
+                width: device.width,
+                height: device.height,
+                cssWidth: device.cssWidth,
+                cssHeight: device.cssHeight,
+                screenWidth: device.screenWidth,
+                screenHeight: device.screenHeight,
+                devicePixelRatio: device.devicePixelRatio,
+                mobile: device.isMobile,
+                userAgent: device.userAgent,
+                language: messages.defaultLanguageCode,
+                userTime: Date.now(),
+                userTimeString: Date(),
+                location: location.href,
+                gamePadInfo: collectGamePadInfo(),
+                positionInfo: positionInfo,
+                app: appName,
+                version: appVersion
+            };
+
+            events.fire(Event.ANALYTICS, payload);
+        });
     }
 
     return sendSystemEvent;
-})(H5.Event, window.location, window.appName, window.appVersion);
+})(H5.Event, window.location, window.appName, window.appVersion, window.navigator.geolocation, H5.Promise, H5.GamePad);
