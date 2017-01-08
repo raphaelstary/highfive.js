8aba021e29c9f5e70b7eebb250b5ce4425cd2f5e
raphaelstary <raphael.stary@gmail.com>
2017-01-08 09:10:26 -0800

    fix undefined issue with errors #core
    (cherry picked from commit 94dc67e)

--- www/js/highfivejs/analytics/bootstrap/installHolmes.js
+++ www/js/highfivejs/analytics/bootstrap/installHolmes.js
@@ -4,17 +4,22 @@ H5.installHolmes = (function (Event, HolmesConnector, window) {
     function installHolmes(url, tenantCode, appKey, events) {
         var gOldOnError = window.onerror;
         window.onerror = function myErrorHandler(errorMsg, url, lineNumber, columnNumber, error) {
-            events.fire(Event.ANALYTICS, {
+            var payload = {
                 type: 'error',
                 msg: errorMsg,
                 url: url,
                 line: lineNumber,
-                column: columnNumber,
-                error: {
+                column: columnNumber
+            };
+
+            if (error) {
+                payload.error = {
                     message: error.message,
                     stack: error.stack
-                }
-            });
+                };
+            }
+
+            events.fire(Event.ANALYTICS, payload);
 
             if (gOldOnError) {
                 // Call previous handler.
