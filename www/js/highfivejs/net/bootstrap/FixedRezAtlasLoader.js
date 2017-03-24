714ef37612010c17e2c520a5a59bd2ad8da33a6c
Raphael Stary <raphael.stary@gmail.com>
2017-03-24 12:25:00 -0700

    height information not needed when there is only one file #core
    (cherry picked from commit 2b64c7b)

--- www/js/highfivejs/net/bootstrap/FixedRezAtlasLoader.js
+++ www/js/highfivejs/net/bootstrap/FixedRezAtlasLoader.js
@@ -25,7 +25,7 @@ H5.FixedRezAtlasLoader = (function (AtlasResourceHelper, createAtlasPaths, Devic
         load: function (resourceLoader) {
             var isMobile = new Device(userAgent, 1, 1, 1).isMobile;
             AtlasResourceHelper.register(resourceLoader, atlases, isMobile,
-                createAtlasPaths(baseName, gfxPath, dataPath, gfxExt, dataExt).add(fixedHeight).getResolver());
+                createAtlasPaths(baseName, gfxPath, dataPath, gfxExt, dataExt).add().getResolver());
         },
         process: function (services) {
             services.gfxCache = AtlasResourceHelper.processFixedRez(atlases, fixedWidth, fixedHeight);
