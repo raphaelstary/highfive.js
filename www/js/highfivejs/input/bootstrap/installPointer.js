12cb8ea857f44175852bd2e1a217cf36b42b1884
Raphael Stary <raphael.stary@gmail.com>
2017-04-04 17:54:36 -0700

    remove prefixed pointer api code #core
    (cherry picked from commit 4d9e54b)

--- www/js/highfivejs/input/bootstrap/installPointer.js
+++ www/js/highfivejs/input/bootstrap/installPointer.js
@@ -4,31 +4,12 @@ H5.installPointer = (function (PointerHandler, Event, window) {
     function installPointer(events, device, canvas) {
         var pointerHandler = new PointerHandler(events, device);
 
-        if (device.isWin7 && device.isIE10) {
-            if ('ontouchstart' in window) {
-                canvas.addEventListener('touchstart', pointerHandler.touchStart.bind(pointerHandler));
-                canvas.addEventListener('touchmove', pointerHandler.touchMove.bind(pointerHandler));
-                canvas.addEventListener('touchend', pointerHandler.touchEnd.bind(pointerHandler));
-                canvas.addEventListener('touchcancel', pointerHandler.touchCancel.bind(pointerHandler));
-            }
-            canvas.addEventListener('mousedown', pointerHandler.mouseDown.bind(pointerHandler));
-            canvas.addEventListener('mousemove', pointerHandler.mouseMove.bind(pointerHandler));
-            canvas.addEventListener('mouseup', pointerHandler.mouseUp.bind(pointerHandler));
-            canvas.addEventListener('mouseout', pointerHandler.mouseCancel.bind(pointerHandler));
-
-        } else if (window.PointerEvent) {
+        if (window.PointerEvent) {
 
             canvas.addEventListener('pointerdown', pointerHandler.pointerDown.bind(pointerHandler));
             canvas.addEventListener('pointermove', pointerHandler.pointerMove.bind(pointerHandler));
             canvas.addEventListener('pointerup', pointerHandler.pointerUp.bind(pointerHandler));
             canvas.addEventListener('pointerout', pointerHandler.pointerCancel.bind(pointerHandler));
-
-        } else if (window.MSPointerEvent) {
-
-            canvas.addEventListener('MSPointerDown', pointerHandler.pointerDown.bind(pointerHandler));
-            canvas.addEventListener('MSPointerMove', pointerHandler.pointerMove.bind(pointerHandler));
-            canvas.addEventListener('MSPointerUp', pointerHandler.pointerUp.bind(pointerHandler));
-            canvas.addEventListener('MSPointerOut', pointerHandler.pointerCancel.bind(pointerHandler));
 
         } else {
             if ('ontouchstart' in window) {
--- www/js/highfivejs/platform/Device.js
+++ www/js/highfivejs/platform/Device.js
@@ -4,11 +4,8 @@ H5.Device = (function (window, Math) {
     function Device(userAgent, width, height, devicePixelRatio, screenWidth, screenHeight) {
         this.userAgent = userAgent;
         this.isFirefox = /firefox/i.test(userAgent);
-        this.isIE10 = /MSIE 10/i.test(userAgent);
-        this.isWin7 = /Windows NT 6.1/i.test(userAgent);
         this.isIE = /trident/i.test(userAgent);
         this.isEdge = /edge/i.test(userAgent);
-        this.isWiiU = 'wiiu' in window;
         this.isMobile = /mobile/i.test(userAgent);
         this.width = Math.floor(width * devicePixelRatio);
         this.height = Math.floor(height * devicePixelRatio);
