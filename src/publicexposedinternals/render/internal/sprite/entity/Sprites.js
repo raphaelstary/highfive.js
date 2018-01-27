3b889882208c34dd07ceffa4e3b19a978ad37f7e
Raphael Stary <raphael.stary@gmail.com>
2018-01-27 14:34:56 -0800

    fixing a frame number issue (issues with animations longer than 100 frames)

--- src/publicexposedinternals/render/internal/sprite/entity/Sprites.js
+++ src/publicexposedinternals/render/internal/sprite/entity/Sprites.js
@@ -4,11 +4,18 @@ H5.Sprites = (function (Sprite) {
     function createNew(gfxCache, imgPath, numberOfFrames, loop) {
         var frames = [];
         for (var i = 0; i < numberOfFrames; i++) {
-            if (i < 10) {
 
+            if (i < 10) {
                 frames.push(gfxCache.get(imgPath + '_000' + i));
-            } else {
+
+            } else if (i < 100) {
                 frames.push(gfxCache.get(imgPath + '_00' + i));
+
+            } else if (i < 1000) {
+                frames.push(gfxCache.get(imgPath + '_0' + i));
+
+            } else {
+                frames.push(gfxCache.get(imgPath + '_' + i));
             }
         }
         return new Sprite(frames, loop);
