5adabdeca5cabedd8b52e9830e82b9fbabefe965
raphael <raphael.stary@gmail.com>
2018-02-01 15:14:58 -0800

    add color utils

---
+++ src/utils/Colors.js
@@ -0,0 +1,20 @@
+H5.Colors = (function () {
+    'use strict';
+
+    return {
+        toHSL: function toHsl(hue, saturation, brightness) {
+            var SB = {
+                s: saturation / 100,
+                b: brightness / 100
+            };
+            var SL = {
+                s: 0,
+                l: 0
+            };
+            SL.l = (2 - SB.s) * SB.b / 2;
+            SL.s = SL.l && SL.l < 1 ? SB.s * SB.b / (SL.l < 0.5 ? SL.l * 2 : 2 - SL.l * 2) : SL.s;
+
+            return 'hsl(' + hue + ',' + (SL.s * 100 | 0) + '%,' + (SL.l * 100 | 0) + '%)';
+        }
+    };
+})();
