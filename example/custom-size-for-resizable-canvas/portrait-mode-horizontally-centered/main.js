c0dc44c029644aa64e5b7e1e9baeed7ddeb3b1f0
raphael <raphael.stary@gmail.com>
2018-04-20 14:16:14 -0700

    extract pointer input from custom-sized-canvas example to its own - trying to enforce one feature per example for basic examples

--- example/custom-size-for-resizable-canvas/portrait-mode-horizontally-centered/main.js
+++ example/custom-size-for-resizable-canvas/portrait-mode-horizontally-centered/main.js
@@ -26,46 +26,11 @@ window.onload = function () {
                 }
             });
         })
-        .pointer()
         .build(function (services) {
-            var helloWorld = services.stage.createText('Hello World :)')
+            services.stage.createText('Hello World :)')
                 .setColor('white')
                 .setSize(H5.Font.get(20))
                 .setPosition(H5.Width.HALF, H5.Height.HALF);
-
-            var pushed = false;
-            var pushingPointerId = -1;
-            services.events.subscribe(H5.Event.POINTER, function (pointer) {
-                if (!pushed && pointer.type == 'down' && H5.isHit(pointer, helloWorld)) {
-                    helloWorld.setColor('#519657');
-                    pushed = true;
-                    pushingPointerId = pointer.id;
-                    return;
-                }
-
-                if (!pushed && pointer.type == 'move' && H5.isHit(pointer, helloWorld)) {
-                    helloWorld.setColor('#519657');
-                    pushed = true;
-                    pushingPointerId = pointer.id;
-                    return;
-                }
-
-                if (pushed && pointer.id == pushingPointerId && pointer.type == 'up') {
-                    helloWorld.setColor('white');
-                    pushed = false;
-                    pushingPointerId = -1;
-                    return;
-                }
-
-                if (pushed && pointer.id == pushingPointerId && pointer.type == 'move' && !H5.isHit(pointer,
-                        helloWorld)) {
-
-                    helloWorld.setColor('white');
-                    pushed = false;
-                    pushingPointerId = -1;
-                    // return;
-                }
-            });
         })
         .start();
 };
---
+++ example/pointer-input/main.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+    'use strict';
+
+    H5.Bootstrapper
+        .pointer()
+        .build(function (services) {
+            var helloWorld = services.stage.createText('touch me, click me, tap me :)')
+                .setColor('white')
+                .setSize(H5.Font.get(20))
+                .setPosition(H5.Width.HALF, H5.Height.HALF);
+
+            var pushed = false;
+            var pushingPointerId = -1;
+            services.events.subscribe(H5.Event.POINTER, function (pointer) {
+                if (!pushed && pointer.type == 'down' && H5.isHit(pointer, helloWorld)) {
+                    helloWorld.setColor('#519657');
+                    pushed = true;
+                    pushingPointerId = pointer.id;
+                    return;
+                }
+
+                if (!pushed && pointer.type == 'move' && H5.isHit(pointer, helloWorld)) {
+                    helloWorld.setColor('#519657');
+                    pushed = true;
+                    pushingPointerId = pointer.id;
+                    return;
+                }
+
+                if (pushed && pointer.id == pushingPointerId && pointer.type == 'up') {
+                    helloWorld.setColor('white');
+                    pushed = false;
+                    pushingPointerId = -1;
+                    return;
+                }
+
+                if (pushed && pointer.id == pushingPointerId && pointer.type == 'move' && !H5.isHit(pointer,
+                        helloWorld)) {
+
+                    helloWorld.setColor('white');
+                    pushed = false;
+                    pushingPointerId = -1;
+                    // return;
+                }
+            });
+        })
+        .start();
+};
