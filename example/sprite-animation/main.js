c70e50ce8d94cc88257c5099f1424844005d79fb
raphael <raphael.stary@gmail.com>
2018-05-07 18:40:09 -0700

    example for new sprite animation api

--- example/sprite-animation/main.js
+++ example/sprite-animation/main.js
@@ -12,7 +12,25 @@ window.onload = function () {
         .build(function (services) {
             var title = services.stage.createImage('animation-test_0000')
                 .setPosition(H5.Width.HALF, H5.Height.HALF);
-            title.animate('animation-test', 110);
+            var spriteAnimation = title.animate('animation-test', 110)
+                .setCallback(function () {
+                    // console.log('1st run is over');
+
+                    services.timer.in(30, function () {
+                        // console.log('force end of looping sprite');
+                        spriteAnimation.finish();
+
+                        // console.log('let\'s over');
+                        title.animate('animation-test', 110)
+                            .setLoop(false)
+                            .setCallback(function () {
+
+                                // console.log('one more time - now forever');
+                                title.animate('animation-test', 110);
+
+                            });
+                    });
+                });
         })
         .start();
 };
