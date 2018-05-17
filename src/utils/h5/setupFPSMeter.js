25bf09b43378b0c48f9b5b47ce41889e76a239be
raphael <raphael.stary@gmail.com>
2018-05-17 11:11:00 -0700

    make text color editable

--- src/utils/h5/setupFPSMeter.js
+++ src/utils/h5/setupFPSMeter.js
@@ -1,7 +1,7 @@
 H5.setupFPSMeter = (function (Event, Stats, Width, Height, Font) {
     'use strict';
 
-    function setupFPSMeter(services) {
+    function setupFPSMeter(services, optionalColor) {
         services.events.subscribe(Event.TICK_START, Stats.start);
         services.events.subscribe(Event.TICK_END, Stats.end);
 
@@ -13,13 +13,16 @@ H5.setupFPSMeter = (function (Event, Stats, Width, Height, Font) {
         var ms = services.visuals.createText('0')
             .setPosition(Width.get(1920, 1880), Height.get(1080, 1020))
             .setSize(Font.get(50))
-            .setZIndex(111)
-            .setColor('grey');
+            .setZIndex(111);
         var fps = services.visuals.createText('0')
             .setPosition(Width.get(1920, 1880), Height.get(1080, 1040))
             .setSize(Font.get(50))
-            .setZIndex(111)
-            .setColor('grey');
+            .setZIndex(111);
+
+        if (optionalColor) {
+            ms.setColor(optionalColor);
+            fps.setColor(optionalColor);
+        }
 
         services.events.subscribe(Event.TICK_DRAW, function () {
             ms.data.msg = Stats.getMs()
